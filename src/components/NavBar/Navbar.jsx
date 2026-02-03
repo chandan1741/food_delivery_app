bf1fb49847cee84d094f7ac6f3695d113d806557
chandan1741 <chandan1741@gmail.com>
2026-02-03 19:46:12 +0530

    login and cart

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-const Nevbar = () => {
+const Nevbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
 
   return (
     <>
       <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
         <ul className="navbar-menu">
           <Link to='/' onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
           <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
@@ -18,13 +18,13 @@ const Nevbar = () => {
         <div className='navbar-right'>
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
+            <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
             <div className="dot">
 
             </div>
             
           </div>
-          <button>sign in</button>
+          <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
         
       </div>
