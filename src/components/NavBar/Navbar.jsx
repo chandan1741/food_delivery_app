c1ccff53f8e2f84cc0819fdb45a0e9e1e2db136c
chandan1741 <chandan1741@gmail.com>
2026-02-09 19:03:32 +0530

    cart

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 const Nevbar = ({setShowLogin}) => {
-  const [menu, setMenu] = useState("home");
+  const [menu, setMenu] = useState("menu");
+  const {getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <>
@@ -19,7 +21,7 @@ const Nevbar = ({setShowLogin}) => {
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon">
             <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-            <div className="dot">
+            <div className={getTotalCartAmount()===0?"":"dot"}>
 
             </div>
             
